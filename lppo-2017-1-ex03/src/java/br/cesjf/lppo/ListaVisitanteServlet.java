794ddf3b57071e3313002eae2193fe199fe797d1
tiagonogueiraa <tiagonogueirajf@gmail.com>
2017-04-03 21:17:35 -0300

    commit ok?

--- lppo-2017-1-ex03/src/java/br/cesjf/lppo/ListaVisitanteServlet.java
+++ lppo-2017-1-ex03/src/java/br/cesjf/lppo/ListaVisitanteServlet.java
@@ -7,6 +7,15 @@ package br.cesjf.lppo;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,13 +33,38 @@ public class ListaVisitanteServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	    throws ServletException, IOException {
 	
+	//criar lista
+	List<Visitante> visitantes = new ArrayList<>();
+	
+	//pegar os dados do banco
+	try {
+	    //configurando conexao
+	    Class.forName("org.apache.derby.jdbc.ClientDriver");
+	    Connection conexao = DriverManager.getConnection("jdbc:derby://localhost:1527/lppo-2017-1", "usuario" , "senha");
+	    Statement operacao = conexao.createStatement();
+	    ResultSet resultado = operacao.executeQuery("SELECT * FROM visitante");
+
+	    while(resultado.next()){
+		Visitante v = new Visitante();
+		v.setId(resultado.getLong("id"));
+		v.setNome(resultado.getString("nome"));
+		v.setIdade(resultado.getInt("idade"));
+		v.setEntrada(resultado.getDate("entrada"));
+		v.setSaida(resultado.getDate("saida"));
+		visitantes.add(v);	
+		
+	    }
+	    
+	} catch (ClassNotFoundException ex) {
+	    Logger.getLogger(ListaVisitanteServlet.class.getName()).log(Level.SEVERE, null, ex);
+	} catch (SQLException ex) {
+	    Logger.getLogger(ListaVisitanteServlet.class.getName()).log(Level.SEVERE, null, ex);
+	}
+	
+	request.setAttribute("visitantes", visitantes);
+	request.getRequestDispatcher("WEB-INF/ListaVisitante.jsp").forward(request, response);
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-	    throws ServletException, IOException {
-
-    }
 
 
 }
---
+++ lppo-2017-1-ex03/web/WEB-INF/ListaVisitante.jsp
@@ -0,0 +1,40 @@
+<%-- 
+    Document   : ListaVisitante
+    Created on : 02/04/2017, 16:27:40
+    Author     : tiago
+--%>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Lista Visitante</title>
+    </head>
+    <body>
+        <h1>Lista Visitante</h1><table border="1">
+	    <thead>
+		<tr>
+		    <th>ID</th>
+		    <th>Nome</th>
+		    <th>Idade</th>
+		    <th>Entrada</th>
+		    <th>Saida</th>
+		</tr>
+	    </thead>
+	    <c:forEach var="visitante" item="${visitantes}">
+	    <tbody>
+		<tr>
+		    <td>${visitante.id}</td>
+		    <td>${visitante.nome}</td>
+		    <td>${visitante.idade}</td>
+		    <td>${visitante.entrada}</td>
+		    <td>${visitante.saida}</td>
+		</tr>
+	    </c:forEach>
+	    </tbody>
+	</table>
+
+    </body>
+</html>
